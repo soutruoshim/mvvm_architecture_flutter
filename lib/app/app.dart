1ed2aa197a72c251eebd755e7f54a7f96bc9d265
srhdp <srhdp@MacBook-Pro.local>
2024-10-07 09:58:05 +0700

    Create Application Class

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal(); // private named constructor
+
+  static final MyApp instance = MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance; // factory for the class instance
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
