c73b29a773d38c587db9c2b0716dafb7d8350535
srhdp <srhdp@MacBook-Pro.local>
2024-10-16 11:09:00 +0700

    Implementation Onboarding Screen

--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -15,6 +15,7 @@ class AppPadding {
   static const double p20 = 20.0;
 }
 class AppSize {
+  static const double s0 = 0;
   static const double s1_5 = 1.5;
   static const double s4 = 4.0;
   static const double s8 = 8.0;
@@ -24,6 +25,11 @@ class AppSize {
   static const double s18 = 18.0;
   static const double s20 = 20.0;
   static const double s28 = 28.0;
+  static const double s40 = 40.0;
   static const double s60 = 60.0;
   static const double s65 = 65.0;
+  static const double s100 = 100.0;
+}
+class DurationConstant{
+  static const int d300 = 300;
 }
