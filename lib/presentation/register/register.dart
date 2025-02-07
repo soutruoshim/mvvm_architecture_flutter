80ba0d2a3bb06481f7f6a3d948135c26c751215f
srhdp <srhdp@MacBook-Pro.local>
2025-02-07 09:38:19 +0700

    view model with stream

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
+
   @override
   State<RegisterView> createState() => _RegisterViewState();
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
---
+++ lib/presentation/register/register_viewmodel.dart
@@ -0,0 +1,131 @@
+import 'dart:async';
+import 'dart:io';
+import '../../app/functions.dart';
+import '../../domain/usecase/register_usecase.dart';
+import '../base/baseviewmodel.dart';
+
+class RegisterViewModel extends BaseViewModel
+    implements RegisterViewModelInput, RegisterViewModelOutput {
+  StreamController _userNameStreamController =
+  StreamController<String>.broadcast();
+  StreamController _mobileNumberStreamController =
+  StreamController<String>.broadcast();
+  StreamController _emailStreamController =
+  StreamController<String>.broadcast();
+  StreamController _passwordStreamController =
+  StreamController<String>.broadcast();
+  StreamController _profilePictureStreamController =
+  StreamController<File>.broadcast();
+  StreamController _isAllInputsValidStreamController =
+  StreamController<void>.broadcast();
+
+  RegisterUseCase _registerUseCase;
+  RegisterViewModel(this._registerUseCase);
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+  @override
+  void dispose() {
+    _isAllInputsValidStreamController.close();
+    _userNameStreamController.close();
+    _mobileNumberStreamController.close();
+    _emailStreamController.close();
+    _passwordStreamController.close();
+    _profilePictureStreamController.close();
+    super.dispose();
+  }
+
+  @override
+  Sink get inputEmail => _emailStreamController.sink;
+  @override
+  Sink get inputMobileNumber => _mobileNumberStreamController.sink;
+  @override
+  Sink get inputProfilePicture => _profilePictureStreamController.sink;
+  @override
+  Sink get inputUPassword => _passwordStreamController.sink;
+  @override
+  Sink get inputUserName => _userNameStreamController.sink;
+
+
+  // output
+  @override
+  Stream<bool> get outputIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
+
+  @override
+  Stream<String?> get outputErrorUserName => outputIsUserNameValid
+      .map((isUserNameValid) => isUserNameValid ? null : "Invalid username");
+
+  @override
+  Stream<bool> get outputIsEmailValid =>
+      _emailStreamController.stream.map((email) => isEmailValid(email));
+
+  @override
+  Stream<String?> get outputErrorEmail => outputIsEmailValid
+      .map((isEmailValid) => isEmailValid ? null : "Invalid Email");
+
+  @override
+  Stream<bool> get outputIsMobileNumberValid =>
+      _mobileNumberStreamController.stream
+          .map((mobileNumber) => _isMobileNumberValid(mobileNumber));
+
+  @override
+  Stream<String?> get outputErrorMobileNumber =>
+      outputIsMobileNumberValid.map((isMobileNumberValid) =>
+      isMobileNumberValid ? null : "Invalid Mobile Number");
+
+  @override
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
+
+  @override
+  Stream<String?> get outputErrorPassword => outputIsPasswordValid
+      .map((isPasswordValid) => isPasswordValid ? null : "Invalid Password");
+
+  @override
+  Stream<File> get outputIsProfilePictureValid =>
+      _profilePictureStreamController.stream.map((file) => file);
+
+  @override
+  register() {
+    throw UnimplementedError();
+  }
+
+  // -- private methods
+  bool _isUserNameValid(String userName) {
+    return userName.length >= 8;
+  }
+  bool _isMobileNumberValid(String mobileNumber) {
+    return mobileNumber.length >= 10;
+  }
+  bool _isPasswordValid(String password) {
+    return password.length >= 8;
+  }
+
+}
+
+abstract class RegisterViewModelInput {
+  register();
+  Sink get inputUserName;
+  Sink get inputMobileNumber;
+  Sink get inputEmail;
+  Sink get inputUPassword;
+  Sink get inputProfilePicture;
+}
+abstract class RegisterViewModelOutput {
+  Stream<bool> get outputIsUserNameValid;
+  Stream<String?> get outputErrorUserName;
+
+  Stream<bool> get outputIsMobileNumberValid;
+  Stream<String?> get outputErrorMobileNumber;
+
+  Stream<bool> get outputIsEmailValid;
+  Stream<String?> get outputErrorEmail;
+
+  Stream<bool> get outputIsPasswordValid;
+  Stream<String?> get outputErrorPassword;
+
+  Stream<File> get outputIsProfilePictureValid;
+}
